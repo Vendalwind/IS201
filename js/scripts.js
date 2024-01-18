84455c7b5bf518ae24cef0ee094a219cf14e7ab5
Vendalwind <vendalwind@gmail.com>
2024-01-17 22:48:44 -0900

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,38 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollPos = currentTop;
     });
 })
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+});
+//Some attempts at providing a custom JS parallax scrolling, I ultimately decided I was spending too much time trying to get this to work right, and JS is outta the scope of this class. Havent done webdev for awhile so felt kinda good to dabble again
+// function parallaxScroll(){
+//    var scrolled = $(window).scrollTop(); 
+//    $('.layer-1').css('top',(0-(scrolled*.25))+'px');
+//    $('.layer-2').css('top',(0-(scrolled*.5))+'px');
+//    $('.layer-3').css('top',(0-(scrolled*.75))+'px');
+//    $('.rock-1').css('top',(400-(scrolled*.8))+'px');
+//    $('.rock-2').css('top',(200-(scrolled*.6))+'px');
+//    $('.rock-3').css('top',(500-(scrolled*.4))+'px'); 
+//    $('.rock-4').css('top',(600-(scrolled*.5))+'px'); 
+//    $('.rock-5').css('top',(600-(scrolled*.7))+'px'); 
+//    $('.rock-6').css('top',(400-(scrolled*.7))+'px'); 
+//    $('.rock-7').css('top',(600-(scrolled*.5))+'px'); 
+//    $('.rock-8').css('top',(200-(scrolled*.2))+'px');
+//    $('.rock-9').css('top',(200-(scrolled*.4))+'px');
+// }
+// window.addEventListener('scroll', function(event) {
+//     var depth, i, layer, layers, len, movement, topDistance, translate3d;
+//     topDistance = this.pageYOffset;
+//     layers = document.querySelectorAll("[data-type='parallax']");
+//     for (i = 0, len = layers.length; i < len; i++) {
+//       layer = layers[i];
+//       depth = layer.getAttribute('data-depth');
+//       movement = -(topDistance * depth);
+//       translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+//       layer.style['-webkit-transform'] = translate3d;
+//       layer.style['-moz-transform'] = translate3d;
+//       layer.style['-ms-transform'] = translate3d;
+//       layer.style['-o-transform'] = translate3d;
+//       layer.style.transform = translate3d;
+//     }
+//   });
